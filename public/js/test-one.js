068530717d5971e603a2fa7f3cc4e2780d6f4ce2
kinmen <kinmenyang7589@gmail.com>
2016-09-01 21:26:53 -0700

    Draft of landing page, with working form and calculator

--- public/js/test-one.js
+++ public/js/test-one.js
@@ -1,1 +1,18 @@
-console.log('here is file one');
+$(function() {
+	$("#calcform").submit(function(e) {
+		e.preventDefault();
+		var jackpot = $("#jackpot").val();
+		var probability = $("#probability").val();
+		var tickets = $("#tickets").val();
+
+		var j = parseFloat(jackpot.replace(/,/g, ''));
+		var p = parseFloat(probability.replace(/,/g,''));
+		var t = parseFloat(tickets.replace(/,/g, ''));
+
+
+		var ev = (j/t) * (1-Math.exp((-1)*(t)*(p)));
+
+
+		$("#expected-value").val(ev);
+	})
+})
