1b9f3d18a97492231a49db1a2e4cab1201b82e5f
kinmen <kinmenyang7589@gmail.com>
2016-09-27 17:32:12 -0700

    Fixed prob fraction parse, added mathjax to display draft math formula

--- public/js/test-one.js
+++ public/js/test-one.js
@@ -1,12 +1,23 @@
 $(function() {
 
+	var parseFrac = function(x) {
+		var nums = x.split('/');
+		if(nums.length > 1){
+		    return (nums[0] / nums[1]);
+		}
+		else{
+		    return (nums[0]);
+		}
+
+	}
+
 	var updateCalcution = function () {
 		var jackpot = $("#jackpot").val();
 		var probability = $("#probability").val();
 		var tickets = $("#tickets").val();
 
 		var j = parseFloat(jackpot.replace(/,/g, ''));
-		var p = parseFloat(probability.replace(/,/g,''));
+		var p = parseFloat(parseFrac(probability.replace(/,/g,'')));
 		var t = parseFloat(tickets.replace(/,/g, ''));
 
 
